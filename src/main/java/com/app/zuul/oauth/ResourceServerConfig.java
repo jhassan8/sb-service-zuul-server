c9643a3083fd34f36ba706d5cc835fc6a35f9696
Jocs <jhassan8@outlook.com>
2019-09-29 01:52:08 -0300

    move key and clients to server config

--- src/main/java/com/app/zuul/oauth/ResourceServerConfig.java
+++ src/main/java/com/app/zuul/oauth/ResourceServerConfig.java
@@ -1,5 +1,7 @@
 package com.app.zuul.oauth;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,10 +12,14 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+	@Value("config.security.oauth.jwt.key")
+	private String jwtKey;
+	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore());
@@ -42,7 +48,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-		jwtAccessTokenConverter.setSigningKey("temporal_default_key");
+		jwtAccessTokenConverter.setSigningKey(jwtKey);
 		return jwtAccessTokenConverter;
 	}
 	
